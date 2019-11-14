81bc585ea5e741aaeb829122c14af25529a5823e
gabriel <jeronimogbariel@gmail.com>
2019-11-14 15:52:31 -0300

    criando a primeira tela em formato mobile

--- main.py
+++ main.py
@@ -2,11 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from kivy.app import App
+from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+Config.set('graphics', 'width', '340')
+Config.set('graphics', 'height', '640')
 
 
 class MainWid(ScreenManager):
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(**kwargs)
+		self.startWid = StartWid()
+		
+		wid = Screen(name='start')
+		wid.add_widget(self.startWid)
+		self.add_widget(wid)
+
+
+class StartWid(BoxLayout):
 	pass
 
 
